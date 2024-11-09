b3215efcf4be2a90c45e3f6b356c171eb2aa5813
Steven Chu <stevenc1@gmail.com>
2024-11-09 14:26:31 -0800

    Fixes to Py server

--- py-server.py
+++ py-server.py
@@ -1,25 +1,37 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+
+import datetime
 import json
+import threading
+import time
+
+
+first_ts = None
+last_ts = None
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def log_message(self, format, *args) -> None:
+        pass
 
-    def do_GET(self):
-        # Respond to GET requests
+    def do_GET(self) -> None:
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(b"Hello, world! This is a GET response.")
 
-    def do_POST(self):
-        # Respond to POST requests
-        content_length = int(
-            self.headers["Content-Length"]
-        )  # Get the length of the data
-        post_data = self.rfile.read(content_length)  # Read the data
-        print(f"Received POST data: {post_data.decode('utf-8')}")  # Print the data
+    def do_POST(self) -> None:
+        content_length = int(self.headers["Content-Length"])
+        _ = self.rfile.read(content_length)
 
-        # Respond with a success message
+        now = datetime.datetime.now()
+        global first_ts
+        if first_ts is None:
+            first_ts = now
+        global last_ts
+        last_ts = now
+
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
@@ -27,8 +39,22 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(response).encode("utf-8"))
 
 
-# Start the server
-server_address = ("", 8000)  # Listen on all available interfaces, port 8000
-httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
-print("Serving HTTP on port 8000...")
-httpd.serve_forever()
+def report_status() -> None:
+    while True:
+        global first_ts
+        global last_ts
+        print(f"First timestamp: {first_ts}; last timestamp: {last_ts}")
+        time.sleep(30)
+
+
+def main() -> None:
+    server_address = ("", 8000)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print("Serving HTTP on port 8000...")
+    rt = threading.Thread(target=report_status, daemon=True)
+    rt.start()
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
